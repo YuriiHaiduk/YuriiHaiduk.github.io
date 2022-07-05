2071d5bfb610322fa2af8c2f0b24434fd823b2f4
webskylogic@gmail.com <WeBPro37gSk75Y4>
2022-07-05 14:46:39 +0300

    photographer landing

--- LANDINGRESUME/src/js/index.js
+++
@@ -1,8 +0,0 @@
-import '../js/particles.min'
-import '../assets/libs/grid.min.css';
-import '../scss/style.scss';
-import '../js/custom-dropdown';
-import './canvas-line';
-import '../js/product-quantity';
-import '../js/tabs';
-import '../js/main';
